692451af76c59973262d3c15d5942068e36b385e
rudra2111 <63008451+rudra2111@users.noreply.github.com>
2023-11-23 18:46:49 +0000

    call-airtable-api whole data

--- src/server/api/routers/airtable.ts
+++ src/server/api/routers/airtable.ts
@@ -1,22 +1,42 @@
 import Airtable from "airtable";
 
 import { createTRPCRouter, publicProcedure } from "eufs-finance/server/api/trpc";
+import { date, string } from "zod";
+
+// Define the interface for a reimbursement
+interface reimbursements{
+    price: number;
+    team: string;
+    sub_team: string;
+    date: string;
+}
+
 
 const base = new Airtable({apiKey: 'patYgRTIOPRIpKD0D.68f7c75b875a70c115c8a26b80907d60599861c00968e4d42dab2a7e87383f04'}).base('appeXw3HbMoKHSeYM');
 
 export const airtableRouter = createTRPCRouter({
   requests: publicProcedure
     .query( () => {
+        //initialise an array to store reimbursements
+        const rows: reimbursements[] = []; 
          
       base('Requests').select({
-          // Selecting the first 3 records in Table:
-          maxRecords: 3,
           view: "Table"
       }).eachPage((records, fetchNextPage) => {
           // This function (`page`) will get called for each page of records.
 
           records.forEach(function(record) {
-              console.log('Retrieved', record.get('ID'));
+            const row : reimbursements = {
+                price: record.get("Total") as number,
+                team: record.get("Team") as string,
+                sub_team: record.get("Subteam") as string,
+                date: record.get("Date submitted") as string,   
+          };
+            rows.push(row);
+              console.log('Retrieved', record.get('Description'));
+              
+            //convert this to a list and store it there 
+              
           });
 
           // To fetch the next page of records, call `fetchNextPage`.
@@ -25,8 +45,11 @@ export const airtableRouter = createTRPCRouter({
           fetchNextPage();
       }, function done(err) {
           if (err) { console.error(err); }
+          else{
+            console.log('Reimbursements:', rows);
+          }
       });
 
-      return "value";
+      return rows;
     }),
 });
