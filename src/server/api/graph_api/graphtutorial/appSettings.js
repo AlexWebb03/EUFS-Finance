6a44e88ea2e2892c501a1c5b7de3f80a67917a48
bwn <89072276+xxxwbwxxx@users.noreply.github.com>
2024-01-23 19:41:51 +0000

    graph_api skeleton

---
+++ src/server/api/graph_api/graphtutorial/appSettings.js
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+// <SettingsSnippet>
+const settings = {
+  'clientId': 'e575440b-61cb-4f35-b65d-05bc4f9cf7f9',
+  'tenantId': 'common',
+  'graphUserScopes': [
+    'user.read',
+    'mail.read',
+    'mail.send'
+  ]
+};
+
+module.exports = settings;
+// </SettingsSnippet>
